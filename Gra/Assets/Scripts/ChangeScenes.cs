b11c12ba0e45d3198dad1f4a641f0a1fe70b6c47
Wrobel1111 <60431457+Wrobel1111@users.noreply.github.com>
2023-05-30 13:16:45 +0200

    Work 30.05 _2
    UI development, including ingame menu

--- Gra/Assets/Scripts/ChangeScenes.cs
+++ Gra/Assets/Scripts/ChangeScenes.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
+	[SerializeField]
+	string SceneName;
 	public void ChangeScenesFunction()
 	{
-		SceneManager.LoadScene("Game");
+		SceneManager.LoadScene(SceneName);
 	}
 }
